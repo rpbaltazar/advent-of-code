401429fcd6df947055e2c6f8f4a1792c6d64b742
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-12-12 12:38:53 +0800

    WIP - solved part 1

--- 2015/day-6/solver.rb
+++ 2015/day-6/solver.rb
@@ -1,117 +1,67 @@
-require_relative '../utils/reader'
+# grid = [][]
+# use true/false for current state of the grid cell
+# (each grid cell represents a light)
 
-def get_instruction(str)
-  coordinates = str.scan(/\d+,\d+/)
-  non_instruction = coordinates.join(' through ')
-  instruction = str.gsub non_instruction, ''
-  {
-    instruction: instruction.strip,
-    coordinates: coordinates
-  }
-end
+# For Part 2, changes needed:
+# - brightness counter - replace the 'false' initialization with '0'
+# - update turn on method to add 1 to brightness counter
+# - update turn off method to reduce 1 to brightness counter. minimum is 0
+# - update toggle method to add 2 to brightness counter
+class Solver
+  attr_reader :grid
 
-def fetch_coordinates(coordinates)
-  start_xy, end_xy = coordinates
-  x1, y1 = start_xy.split(',').map(&:to_i)
-  x2, y2 = end_xy.split(',').map(&:to_i)
-  {
-    start_x: x1,
-    start_y: y1,
-    end_x: x2,
-    end_y: y2
-  }
-end
+  def initialize
+    # setup the data with initial values
+    @grid = Array.new(1000) { Array.new(1000, false) }
+  end
 
-def turn_on1(coordinates)
-  lights_coords = fetch_coordinates coordinates
-  (lights_coords[:start_x]..lights_coords[:end_x]).each do |x|
-    (lights_coords[:start_y]..lights_coords[:end_y]).each do |y|
-      @lights_on[x] ||= {}
-      @lights_on[x][y] = true
+  def solve(input_name)
+    File.foreach(input_name) do |instruction|
+      action = extract_action instruction
+      coordinates = extract_coordinates instruction
+      run_action(action, *coordinates)
+    end
+    grid.flatten.count(true)
+  end
+
+  def turn_on(x, y)
+    grid[x][y] = true
+  end
+
+  def toggle(x, y)
+    if grid[x][y] == true
+      grid[x][y] = false
+    elsif grid[x][y] == false
+      grid[x][y] = true
+    end
+  end
+
+  def turn_off(x, y)
+    grid[x][y] = false
+  end
+
+  def extract_coordinates(instruction)
+    instruction.scan(/\d+/).map(&:to_i)
+  end
+
+  def extract_action(instruction)
+    # solution 1: case/when
+    # solution 2: if/else
+    # There is only one match for sure, so we can safely return index 0
+    action_str = instruction.scan(/turn on|turn off|toggle/)[0]
+    action_str.tr(' ', '_')
+  end
+
+  private
+
+  def run_action(action, x1, y1, x2, y2)
+    (x1..x2).each do |temp_x|
+      (y1..y2).each do |temp_y|
+        send(action, temp_x, temp_y)
+      end
     end
   end
 end
 
-def turn_off1(coordinates)
-  lights_coords = fetch_coordinates coordinates
-  (lights_coords[:start_x]..lights_coords[:end_x]).each do |x|
-    (lights_coords[:start_y]..lights_coords[:end_y]).each do |y|
-      @lights_on[x] ||= {}
-      @lights_on[x][y] = false
-    end
-  end
-end
-
-def toggle1(coordinates)
-  lights_coords = fetch_coordinates coordinates
-  (lights_coords[:start_x]..lights_coords[:end_x]).each do |x|
-    (lights_coords[:start_y]..lights_coords[:end_y]).each do |y|
-      @lights_on[x] ||= {}
-      @lights_on[x][y] = !@lights_on[x][y]
-    end
-  end
-end
-
-def turn_on2(coordinates)
-  lights_coords = fetch_coordinates coordinates
-  (lights_coords[:start_x]..lights_coords[:end_x]).each do |x|
-    (lights_coords[:start_y]..lights_coords[:end_y]).each do |y|
-      @lights_on[x] ||= {}
-      @lights_on[x][y] ||= 0
-      @lights_on[x][y] += 1
-    end
-  end
-end
-
-def turn_off2(coordinates)
-  lights_coords = fetch_coordinates coordinates
-  (lights_coords[:start_x]..lights_coords[:end_x]).each do |x|
-    (lights_coords[:start_y]..lights_coords[:end_y]).each do |y|
-      @lights_on[x] ||= {}
-      @lights_on[x][y] ||= 0
-      @lights_on[x][y] -= 1
-      @lights_on[x][y] = 0 if @lights_on[x][y].negative?
-    end
-  end
-end
-
-def toggle2(coordinates)
-  lights_coords = fetch_coordinates coordinates
-  (lights_coords[:start_x]..lights_coords[:end_x]).each do |x|
-    (lights_coords[:start_y]..lights_coords[:end_y]).each do |y|
-      @lights_on[x] ||= {}
-      @lights_on[x][y] ||= 0
-      @lights_on[x][y] += 2
-    end
-  end
-end
-
-def result1
-  count = 0
-  @lights_on.each do |_x, ys|
-    count += ys.select { |_y, on| on }.length
-  end
-  count
-end
-
-def result2
-  count = 0
-  @lights_on.each do |_x, ys|
-    ys.each { |_y, val| count += val }
-  end
-  count
-end
-
-def init(part)
-  @lights_on = {}
-  File.open('input').each do |string|
-    set = get_instruction string
-    method = set[:instruction].tr(' ', '_')
-    send("#{method}#{part}", set[:coordinates])
-  end
-
-  puts send("result#{part}")
-end
-
-init(1)
-init(2)
+solver = Solver.new
+solver.solve('input_small')
