845f78cda15f9d2b0303df933c41b3b006eed4e2
Rui Baltazar <rui.baltazar@transferwise.com>
2023-12-05 10:53:11 +0800

    add target to gitignore

--- 2023/day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/Users/rui.baltazar/personal-dev/aoc/2023/day2/target/debug/day2: /Users/rui.baltazar/personal-dev/aoc/2023/day2/src/main.rs
--- 2023/day2/target/debug/deps/day2-49c44a93e09b4484.d
+++
@@ -1,5 +0,0 @@
-/Users/rui.baltazar/personal-dev/aoc/2023/day2/target/debug/deps/day2-49c44a93e09b4484: src/main.rs
-
-/Users/rui.baltazar/personal-dev/aoc/2023/day2/target/debug/deps/day2-49c44a93e09b4484.d: src/main.rs
-
-src/main.rs:
